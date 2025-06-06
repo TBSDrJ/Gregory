b46cc86de7945673bfe2abce23a06714b13b0d5f
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-06-06 15:03:19 -0700

    Write a constructor that fixes bugs in my mallocs.

--- polynomials.c
+++ polynomials.c
@@ -7,6 +7,38 @@ struct polynomial {
     long* coeffs;
 };
 
+/*
+I stumbled across this, which had some good suggestions about how to make a
+constructor of sorts, it makes things a little simpler than what I had:
+https://stackoverflow.com/questions/14768230/malloc-for-struct-and-pointer-in-c
+*/
+
+struct polynomial* constructor(long deg) {
+    struct polynomial* p = malloc(sizeof(struct polynomial));
+    if (!p) {
+        return NULL;
+    }
+    // Notice we need 1 *more* than the degree 
+    long* coeffs = malloc((deg + 1) * sizeof(long));
+    if (!coeffs) {
+        return NULL;
+    }
+    // I'm choosing to initialize also
+    for (long i=0; i<=deg; i++) {
+        coeffs[i] = 0;
+    }
+    p -> degree = deg;
+    p -> coeffs = coeffs;
+    return p;
+}
+
+void destructor(struct polynomial* p) {
+    if (!p) {
+        free(p -> coeffs);
+        free(p);
+    }
+}
+
 /* 
 Unary operations:
 1. Derivative
@@ -17,11 +49,9 @@ Binary Operations:
 1. Sum
 2. Product
 */
+
 struct polynomial* der(struct polynomial* p) {
-    struct polynomial* d = (struct polynomial*) malloc(sizeof(long) + 
-            sizeof(size_t));
-    d -> degree = p -> degree - 1;
-    d -> coeffs = (long*) malloc(((p -> degree) - 1)*sizeof(long));
+    struct polynomial* d = constructor((p -> degree) - 1);
     for (long i=0; i<(p -> degree); i++) {
         (d -> coeffs)[i] = (p -> coeffs)[i+1] * (i+1);
     }
@@ -29,10 +59,7 @@ struct polynomial* der(struct polynomial* p) {
 }
 
 struct polynomial* neg(struct polynomial* p) {
-    struct polynomial* n = (struct polynomial*) malloc(sizeof(long) + 
-            sizeof(size_t));
-    n -> degree = p -> degree;
-    n -> coeffs = (long*) malloc((p -> degree)*sizeof(long));
+    struct polynomial* n = constructor(p -> degree);
     for (long i=0; i<((n -> degree) + 1); i++) {
         (n -> coeffs)[i] = -(p -> coeffs)[i];
     }
@@ -54,10 +81,7 @@ struct polynomial* sum(struct polynomial* p, struct polynomial* q) {
         p = q;
         q = temp;
     }
-    struct polynomial* s = (struct polynomial*) malloc(sizeof(long) + 
-            sizeof(size_t));
-    s -> degree = p -> degree;
-    s -> coeffs = (long*) malloc(sizeof(long) * (p -> degree));
+    struct polynomial* s = constructor(p -> degree);
     for (long i=0; i<=(q -> degree); i++) {
         s -> coeffs[i] = (p -> coeffs[i]) + (q -> coeffs[i]);
     }
@@ -68,15 +92,11 @@ struct polynomial* sum(struct polynomial* p, struct polynomial* q) {
 }
 
 int main() {
-    struct polynomial* p = (struct polynomial*) malloc(sizeof(long) + 
-            sizeof(size_t));
-    p -> degree = 3;
-    long* coeffs = (long*) malloc(((p -> degree) + 1)*sizeof(long));
-    coeffs[0] = 0;
-    coeffs[1] = 4;
-    coeffs[2] = -5;
-    coeffs[3] = 1;
-    p -> coeffs = coeffs;
+    struct polynomial* p = constructor(3);
+    p -> coeffs[0] = 0;
+    p -> coeffs[1] = 4;
+    p -> coeffs[2] = -5;
+    p -> coeffs[3] = 1;
     struct polynomial* n = neg(p);
     struct polynomial* d = der(p);
     printf("p(x) = ");
