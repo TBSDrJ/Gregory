a47201e7bb8b045871eff31426a704f7a56c1ed5
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-06-27 09:10:51 -0700

    I had forgotten to #define QUEUE_2_H

--- queue_2.h
+++ queue_2.h
@@ -1,6 +1,9 @@
 /* 
 A struct for a queue/singly-linked list of pairs of pointers to Polynomials.
 */
+#ifndef QUEUE_2_H
+#define QUEUE_2_H
+
 #include<stdlib.h>
 #include"polynomials.h"
 
@@ -23,3 +26,5 @@ void destruct_q_2(struct Queue_2* q);
 struct Entry_2* construct_entry_2(struct Polynomial* a, struct Polynomial* b);
 void destruct_entry_2(struct Entry_2* e);
 void enqueue_2(struct Queue_2* q, struct Polynomial* a, struct Polynomial* b);
+
+#endif
