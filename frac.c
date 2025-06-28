71ad049beeeb6301430907691270b07ce9daeb24
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-06-27 19:13:17 -0700

    Convert queue_long to (my own) Bigint. Set up addition of Bigints.

--- frac.c
+++ frac.c
@@ -1,40 +1,132 @@
+#include<stdbool.h>
 #include"frac.h"
 
-// Functions for Queue_long
-struct Queue_long* construct_queue_long() {
-    struct Queue_long* q = malloc(sizeof(struct Queue_long));
-    q -> head = NULL;
-    q -> tail = NULL;
-    q -> len = 0;
-    return q;
+// Functions for Bigint
+struct Bigint* construct_Bigint() {
+    struct Bigint* b = malloc(sizeof(struct Bigint));
+    b -> sign = 1;
+    b -> head = NULL;
+    b -> tail = NULL;
+    b -> len = 0;
+    return b;
 }
 
-void destruct_queue_long(struct Queue_long* q) {
-    struct Entry_long* e = q -> head;
+void destruct_Bigint(struct Bigint* b) {
+    struct Entry_long* e = b -> head;
     while (e) {e = destruct_entry_long(e);}
-    free(q);
+    free(b);
 }
 
-void enqueue_long(struct Queue_long* q, unsigned long n) {
+void enqueue_to_Bigint(struct Bigint* b, unsigned long n) {
     struct Entry_long* e = construct_entry_long(n);
-    if (q -> tail) {
-        q -> tail -> next = e;
-        q -> tail = e;
-        q -> len++;
+    if (b -> tail) {
+        b -> tail -> next = e;
+        b -> tail = e;
+        b -> len++;
     } else {
-        q -> head = e;
-        q -> tail = e;
-        q -> len = 1;
+        b -> head = e;
+        b -> tail = e;
+        b -> len = 1;
     }
 }
 
-void dump_queue(struct Queue_long* q) {
-    struct Entry_long* e = q -> head;
+void print_Bigint(struct Bigint* b) {
+    if (!b) {
+        return;
+    }
+    struct Entry_long* e = b -> head;
     while(e) {
-        printf("%li", e -> content);
+        printf("%lu, ", e -> content);
         e = e -> next;
     }
     printf("\n");
+}
+
+struct Bigint* add_Bigints(struct Bigint* a, struct Bigint* b) {
+    bool fail = false;
+    if (!(a -> head)) {
+        printf("ERROR: Trying to add empty Bigint, a from add(a,b)\n");
+        fail = true;
+    } 
+    if (!(b -> head)) {
+        printf("ERROR: Trying to add empty Bigint, b from add(a,b)\n");
+        fail = true;
+    }
+    if (fail) {
+        return NULL;
+    }
+    if ((a -> sign == -1) && (b -> sign == 1)) {
+        struct Bigint* neg_a = neg_Bigint(a);
+        // return subtract_Bigints(b, neg_a);
+        return NULL;
+    } else if ((a -> sign == 1) && (b -> sign == -1)) {
+        struct Bigint* neg_b = neg_Bigint(b);
+        // return subtract_Bigints(a, neg_b);
+        return NULL;
+    } 
+    struct Bigint* c = malloc(sizeof(struct Bigint));
+    if ((a -> sign == -1) && (b -> sign == -1)) {
+        c -> sign = -1;
+    }
+    struct Entry_long* entry_a = a -> head;
+    struct Entry_long* entry_b = b -> head;
+    unsigned long a_digit;
+    unsigned long b_digit;
+    // Keep track of top digits to check for and deal with overflow
+    unsigned long sum = 0, top_a, top_b, top_sum, tops;
+    while ((entry_a) || (entry_b)) {
+        if (entry_a) {
+            // Separate top digit from rest of number
+            a_digit = entry_a -> content;
+            top_a = a_digit >> 63;
+            a_digit = a_digit << 1 >> 1;
+            entry_a = entry_a -> next;
+        } else {
+            a_digit = 0;
+            top_a = 0;
+        }
+        if (entry_b) {
+            b_digit = entry_b -> content;            
+            top_b = b_digit >> 63;
+            b_digit = b_digit << 1 >> 1;
+            entry_b = entry_b -> next;
+        } else {
+            b_digit = 0;
+            top_b = 0;
+        }
+        printf("%lu %lu %lu ", a_digit, b_digit, sum);
+        sum += a_digit + b_digit;
+        printf("%lu ", sum);
+        top_sum = sum >> 63;
+        tops = top_a + top_b + top_sum;
+        printf("%lu\n", tops);
+        if (tops > 1) {
+            if (tops == 2) {
+                // In this case, we have a 1 in the 64th bit, but that 1 is
+                //  already accounted for in tops.
+                sum = sum << 1 >> 1;
+            }
+            enqueue_to_Bigint(c, sum);
+            // Start the next sum with the 1 that regroups up to the next place
+            sum = 1;
+        } else {
+            sum += tops << 63;
+            enqueue_to_Bigint(c, sum);
+            sum = 0;
+        }
+    }
+    if (sum == 1) {
+        enqueue_to_Bigint(c, 1);
+    }
+    return c;
+}
+
+struct Bigint* neg_Bigint(struct Bigint* a) {
+    struct Bigint* c = malloc(sizeof(struct Bigint));
+    c -> sign = -1 * (a -> sign);
+    c -> head = a -> head;
+    c -> tail = a -> tail;
+    return c;
 }
 
 // Functions for Entry_long
@@ -52,15 +144,31 @@ struct Entry_long* destruct_entry_long(struct Entry_long* e) {
 }
 
 // Functions for Fraction
-struct Fraction* construct_frac(long numerator, long denominator) {
-    return NULL;
+struct Fraction* construct_frac() {
+    struct Bigint* numerator = construct_Bigint();
+    struct Bigint* denominator = construct_Bigint();
+    struct Fraction* f = malloc(sizeof(struct Fraction));
+    f -> numerator = numerator;
+    f -> denominator = denominator;
+    return f;
 }
 
 int main() {
-    struct Queue_long* q = construct_queue_long();
-    enqueue_long(q, 3141592653);
-    enqueue_long(q, 5897932384);
-    enqueue_long(q, 6264338327);
-    dump_queue(q);
-    destruct_queue_long(q);
+    struct Bigint* a = malloc(sizeof(struct Bigint));
+    struct Bigint* b = malloc(sizeof(struct Bigint));
+    unsigned long n = 0;
+    for (int i=0; i<64; i++) {
+        n += (unsigned long) 1 << i;
+    }
+    printf("%lu\n", n);
+    enqueue_to_Bigint(a, (unsigned long) n);
+    enqueue_to_Bigint(a, (unsigned long) n);
+    enqueue_to_Bigint(a, (unsigned long) n);
+    enqueue_to_Bigint(b, (unsigned long) n);
+    enqueue_to_Bigint(b, (unsigned long) n);
+    struct Bigint* c = add_Bigints(a, b);
+    print_Bigint(c);
+    if (a) {destruct_Bigint(a);}
+    if (b) {destruct_Bigint(b);}
+    if (c) {destruct_Bigint(c);}
 }
--- frac.h
+++ frac.h
@@ -2,6 +2,7 @@
 This constructs an arbitrary-precision fraction of integers.  
 We use a queue of 64-bit unsigned integers to represent 'digits' base 2^64,
 so the i-th entry, n_i, in the queue has value n_i * (2^64)^i, for i>=0.
+NB: This means that 'digits' are ordered from least significant to most.
 */
 #ifndef FRAC_H
 #define FRAC_H
@@ -13,16 +14,22 @@ so the i-th entry, n_i, in the queue has value n_i * (2^64)^i, for i>=0.
 struct Entry_long;
 struct Fraction;
 
-struct Queue_long {
+struct Bigint {
+    // sign should always be +1 or -1.
+    char sign;
     struct Entry_long* head;
     struct Entry_long* tail;
-    long len;
+    unsigned long len;
 };
 
-struct Queue_long* construct_queue_long();
-void destruct_queue_long(struct Queue_long* q);
-void enqueue_long(struct Queue_long* q, unsigned long n);
-void dump_queue(struct Queue_long* q);
+struct Bigint* construct_Bigint();
+void destruct_Bigint(struct Bigint* b);
+void enqueue_to_Bigint(struct Bigint* b, unsigned long n);
+void print_Bigint(struct Bigint* b);
+struct Bigint* add_Bigints(struct Bigint* a, struct Bigint* b);
+struct Bigint* subtract_Bigints(struct Bigint* a, struct Bigint* b);
+// NB: neg_Bigint points to a for its digits, so if a changes, so will its neg!
+struct Bigint* neg_Bigint(struct Bigint* a);
 
 struct Entry_long {
     unsigned long content;
@@ -33,12 +40,10 @@ struct Entry_long* construct_entry_long(unsigned long n);
 struct Entry_long* destruct_entry_long(struct Entry_long* e);
 
 struct Fraction {
-    // sign should always be +1 or -1.
-    char sign;
-    struct Queue_long numerator;
-    struct Queue_long denominator;
+    struct Bigint* numerator;
+    struct Bigint* denominator;
 };
 
-struct Fraction* construct_frac(long numerator, long denominator);
+struct Fraction* construct_frac();
 
 #endif
