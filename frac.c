d03935a588b55bbea8f25ec5f4408c7a09da8cd3
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-09-24 11:50:20 -0700

    Add Fraction_deepcopy

--- frac.c
+++ frac.c
@@ -46,6 +46,19 @@ void Fraction_print(struct Fraction* a) {
     Myint_print(a -> numerator); printf(" / "); Myint_print(a -> denominator);
 }
 
+struct Fraction* Fraction_deepcopy(struct Fraction* a) {
+    if (!(Fraction_contract(a))) {
+        printf("Fraction_contract fails at Fraction_deepcopy\n");
+        return NULL;
+    }
+    struct Fraction* b = Fraction_constructor();
+    Myint_destructor(b -> numerator);
+    Myint_destructor(b -> denominator);
+    b -> numerator = Myint_deepcopy(a -> numerator);
+    b -> denominator = Myint_deepcopy(a -> denominator);
+    return b;
+}
+
 void Fraction_reduce(struct Fraction* a) {
     if (!(Fraction_contract(a))) {
         printf("Fraction_contract fails at Fraction_reduce\n");
@@ -175,36 +188,42 @@ struct Fraction* Fraction_divide(struct Fraction* a, struct Fraction* b) {
 
 int main() {
     struct Fraction* a = Fraction_constructor();
-    struct Fraction* b = Fraction_constructor();
-    struct Fraction* c = Fraction_constructor();
-    struct Fraction* d = Fraction_constructor();
-    struct Fraction* e = Fraction_constructor();
-    a -> numerator -> my_long = 3;
-    a -> denominator -> my_long = 4;
-    b -> numerator -> my_long = 2;
-    b -> denominator -> my_long = 3;
-    struct Myint* two = Myint_constructor();
-    two -> my_long = 2;
-    struct Myint* x = Myint_constructor();
-    x -> my_long = 2;
-    for (int i=0; i<65; i++) {
-        x = Myint_multiply(x, two);
-    }
     a -> numerator -> my_long = 1;
-    a -> denominator = x;
-    b -> numerator = x;
-    b -> denominator -> my_long = 1;
-    c = Fraction_multiply(a, b); Fraction_reduce(c);
-    d = Fraction_divide(a, b); Fraction_reduce(d);
-    e = Fraction_divide(b, a); Fraction_reduce(e);
-    printf("a: "); Fraction_print(a); printf("\n");
-    printf("b: "); Fraction_print(b); printf("\n");
-    printf("a * b: "); Fraction_print(c); printf("\n");
-    printf("a / b: "); Fraction_print(d); printf("\n");
-    printf("b / a: "); Fraction_print(e); printf("\n");
+    a -> denominator -> my_long = 2;
+    struct Fraction* b = Fraction_deepcopy(a);
     a = Fraction_destructor(a);
     b = Fraction_destructor(b);
-    c = Fraction_destructor(c);
-    d = Fraction_destructor(d);
-    e = Fraction_destructor(e);
+    // struct Fraction* a = Fraction_constructor();
+    // struct Fraction* b = Fraction_constructor();
+    // struct Fraction* c = Fraction_constructor();
+    // struct Fraction* d = Fraction_constructor();
+    // struct Fraction* e = Fraction_constructor();
+    // a -> numerator -> my_long = 3;
+    // a -> denominator -> my_long = 4;
+    // b -> numerator -> my_long = 2;
+    // b -> denominator -> my_long = 3;
+    // struct Myint* two = Myint_constructor();
+    // two -> my_long = 2;
+    // struct Myint* x = Myint_constructor();
+    // x -> my_long = 2;
+    // for (int i=0; i<65; i++) {
+    //     x = Myint_multiply(x, two);
+    // }
+    // a -> numerator -> my_long = 1;
+    // a -> denominator = x;
+    // b -> numerator = x;
+    // b -> denominator -> my_long = 1;
+    // c = Fraction_multiply(a, b); Fraction_reduce(c);
+    // d = Fraction_divide(a, b); Fraction_reduce(d);
+    // e = Fraction_divide(b, a); Fraction_reduce(e);
+    // printf("a: "); Fraction_print(a); printf("\n");
+    // printf("b: "); Fraction_print(b); printf("\n");
+    // printf("a * b: "); Fraction_print(c); printf("\n");
+    // printf("a / b: "); Fraction_print(d); printf("\n");
+    // printf("b / a: "); Fraction_print(e); printf("\n");
+    // a = Fraction_destructor(a);
+    // b = Fraction_destructor(b);
+    // c = Fraction_destructor(c);
+    // d = Fraction_destructor(d);
+    // e = Fraction_destructor(e);
 }
